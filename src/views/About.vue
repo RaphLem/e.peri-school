932805da5f65fd7d91b53ca5c564702a16ba5079
FloranMarchal <70960905+FloranMarchal@users.noreply.github.com>
2022-01-04 21:36:14 +0100

    about (ça avance) + image (nom)

--- src/views/About.vue
+++ src/views/About.vue
@@ -9,49 +9,74 @@
     </div>
 
     <div id="programme">
-    <h1>Le programme pédagogique</h1>
+    <h2>Le programme pédagogique</h2>
       <p>Les 10 séances s’articulent autour de deux axes d’apprentissage : la programmation et l’impression 3D.</p>
       <p>Ces deux thématiques sont réparties selon le planning suivant :</p>
 
-      <h2>1. Présentation du programme & découverte des différents composants</h2>
+      <h3>1. Présentation du programme & découverte des différents composants</h3>
       <p>Découvrir les différents composants du kit de programmation : le compteur, la led et le potentiomètre. Réalisation des premiers assemblages simples (sans programmation).</p>
 
-      <h2>2. Programmation : découverte du
-        composant de programmation</h2>
+      <h3>2. Programmation : découverte du
+        composant de programmation</h3>
       <p>Découvrir le composant « programmation » et la plateforme (logiciel)LittleBits Code Bit qui permet de programmer les différents composants. Réaliser des programmes simples : programmer une puissance électrique en sortie, un capteur de lumière et un bouton buzzer.</p>
 
-      <h2>3. Programmation : découverte du
-        composant « moteur »</h2>
+      <h3>3. Programmation : découverte du
+        composant « moteur »</h3>
       <p>Découvrir le fonctionnement du composant « moteur ». Commencer à programmer le moteur en avant et en arrière. Réaliser un assemblage de composants plus complexes avec le potentiomètre, l’afficheur numérique, l’alarme et le détecteur de lumière (ex : si la lumière est inférieur à 50, l’alarme se déclenche).</p>
 
-      <h2>4. Conception 3D</h2>
+      <h3>4. Conception 3D</h3>
       <p>Découvrir l’outil de modélisation Tinkercad. Concevoir des formes géométriques simples pour découvrir les différentes fonctionnalités de la plateforme.</p>
 
-      <h2>5. Conception 3D</h2>
+      <h3>5. Conception 3D</h3>
       <p>Concevoir de formes plus complexes à partir de formes simples. Maitriser davantage l’outil de modélisation.</p>
 
-      <h2>6. Impression 3D</h2>
+      <h3>6. Impression 3D</h3>
       <p>Créer et modéliser une forme encore plus complexe : créer un pot à crayons personnalisée avec perçage.</p>
 
-      <h2>7. Impression 3D</h2>
+      <h3>7. Impression 3D</h3>
       <p>Découvrir le fonctionnement d’une imprimante 3D. Découvrir la plateforme XYZmaker qui permet d’imprimer les éléments modélisés en amont.</p>
 
-      <h2>8. Programmation : assemblage de
-        composants avec boucles et conditions</h2>
+      <h3>8. Programmation : assemblage de
+        composants avec boucles et conditions</h3>
       <p>Monter l’Autobot et programmer les moteurs par le biais de boucles et de conditions pour qu’il se déplace en avant, en arrière, et effectue des virages.</p>
 
-      <h2>9. Programmation : assemblage de
-        composants avec boucles et conditions</h2>
+      <h3>9. Programmation : assemblage de
+        composants avec boucles et conditions</h3>
       <p>Programmer des parcours de l’Autobot (en O, en U et en 8).</p>
 
-      <h2>10. Assemblage final du projet</h2>
+      <h3>10. Assemblage final du projet</h3>
       <p>Programmer des parcours complexes de l’autobot (ex : détection d’obstacles)</p>
     </div>
     <div id="acteurs">
-      <h1>Les acteurs</h1>
+      <h2>Les acteurs</h2>
+      <p>Un projet réalisé par</p>
+      <div>
+        <h3>Numérica SEM</h3>
+        <ul>
+          <li>Maitrise d'ouvrage</li>
+          <li>Ingénierie pédagogique</li>
+          <li>Conception de l'application</li>
+          <li>Formation</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Le pavillon des sciences</h3>
+        <ul>
+          <li>Formation</li>
+          <li>Gestion des impressions 3D</li>
+          <li>Promotion de l'action</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Les Francas su Doubs</h3>
+        <ul>
+          <li>Animation</li>
+        </ul>
+      </div>
     </div>
     <div id="soutiens">
       <h1>Nos soutiens</h1>
+      <p>Sous la maitrise d'ouvrage et avec le soutien de</p>
     </div>
   </div>
 </template>
